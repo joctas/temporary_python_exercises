c6ce8ae983302ec6974230888b4ad371c1988a53
João Octávio Souto Santiago <jomsoutosantiago@gmail.com>
2023-11-10 03:03:10 -0300

    Delete chapter 2 directory

--- chapter 2/exercise_3-3.py
+++
@@ -1,9 +0,0 @@
-
-bill = float(input("Bill: "))
-tip = float(input("Tip: "))
-calculated_tip = bill * (tip / 100)
-total = bill + calculated_tip
-
-print(f"Bill: {bill}")
-print(f"Tip: {tip}%")
-print(f"Total: ${'%.2f' % total}")
--- chapter 2/exercise_4-1.py
+++
@@ -1,6 +0,0 @@
-
-for num in range(1, 11):
-    if num % 2 == 0:
-        print(f"{num} is even")
-    else:
-        print(f"{num} is odd")
