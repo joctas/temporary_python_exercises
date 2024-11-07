6de40aee1442ad1a568bfa07557a468543245705
João Octávio <jomsoutosantiago@gmail.com>
2024-11-06 23:16:42 -0300

    Update exercise_5-4.py
    Fixed a typo

--- chapter_5/exercise_5-4.py
+++ chapter_5/exercise_5-4.py
@@ -5,7 +5,7 @@ count = 0
 
 for number in numbers:
     # checks if the amount of times a number appears in the list is higher than the value of count
-    if numbers.count(number) > 0:
+    if numbers.count(number) > count:
         count = numbers.count(number)
         most_repeated_number = number
 
